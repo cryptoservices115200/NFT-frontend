d273c5de95c38cc6699e09ec7d7a3133c85b29bf
manager <productmanager456@outlook.com>
2022-02-08 00:40:39 -0500

    initial commit

---
+++ src/pages/Mint/index.tsx
@@ -0,0 +1,53 @@
+import Box from "../../components/common/Box";
+import Button from "../../components/common/Button";
+import Flex from "../../components/common/Flex";
+
+import { useContext, useEffect, useState } from "react";
+import { useWeb3React } from "@web3-react/core";
+import { injected } from "../../components/wallet/connectors";
+
+const Mint = () => {
+
+    const { active, account, library, connector, activate, deactivate } = useWeb3React();
+
+    function connect() {
+        activate(injected, async (error) => {
+            console.log(error);
+        });
+      }
+    
+    async function disconnect() {
+        try {
+            deactivate()
+        } catch (ex) {
+            console.log(ex)
+        }
+    }
+
+    return (
+        <Box cssClasses={["section-pad-top", "bg-dark1"]}>
+            <Flex
+                cssClasses={["container"]}
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <h1>Mint</h1>
+                <p className="mt-1 text-center">
+                    Our collection of unique NFTs that can be bought and sold on our
+                    marketplace..
+                </p>
+                <Box cssClasses={["mt-4", "pt-4"]}>
+                    {/* <Button secondary text="Connect Wallet!" /> */}
+                        {active ? 
+                            <Button secondary text={`${account}`} />
+                        : 
+                            <Button secondary text="Connect Wallet !!!!!!" onClick={connect} />
+                        }
+                </Box>
+            </Flex>
+        </Box>
+    );
+};
+
+export default Mint;
