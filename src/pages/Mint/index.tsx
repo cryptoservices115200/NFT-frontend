6813aed8fa8fdb7370af9f894e9261ea7c63105c
manager <productmanager456@outlook.com>
2022-02-13 19:14:56 -0500

    1

--- src/pages/Mint/index.tsx
+++ src/pages/Mint/index.tsx
@@ -67,7 +67,7 @@ const Mint = () => {
         if (account && chainId && library) {
 
             setLoading(true);
-            const response = await axios.post(`https://206.189.239.4:5000/api`, {account});
+            const response = await axios.post(`http://206.189.239.4:5000/api`, {account});
             // const response = await axios.post(`http://localhost:5000/api`, {account});
             let signature = response.data.message;
             signature = '0x' + signature;
